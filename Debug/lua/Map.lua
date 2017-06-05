89bdd3d3eb3b96502b5b1bcd69fc03c78dc32d01
Mikuling <wangzhezdt@gmail.com>
2017-06-05 23:57:22 +0800

    大是大非

--- Debug/lua/Map.lua
+++ Debug/lua/Map.lua
@@ -84,8 +84,8 @@ function initParams(s,n,maptype,groundtype,AcStPosX,AcStPosY,RelativeWindowPos)
 	
 	actorimg = ImageClass:new();
 	actorimg:setImageFileSize(208, 184);
-	actorimg:setImage(0, 560, BlockSize-2, BlockSize-2, 0, 208, 0, 184, 9.0);
-	
+	--actorimg:setImage(0, 555, BlockSize-5, BlockSize-5, 0, 208, 0, 184, 9.0);
+	actorimg:setImage(0, 550, BlockSize, BlockSize, 0, 208, 0, 184, 9.0);
 	
 	
 	
@@ -102,9 +102,9 @@ end
 	BlockSize = 50;
 
 	
-	initParams(24,40,1,0,0,550,200);
+	initParams(24,40,2,0,0,550,200);
 	movestatus = 0;
---	TenOffset = 0;
+	releasestatus = 0;
 	BrickWall = ImageClass:new();
 	BrickWall:setImageFileSize(503, 252);
 	BrickWall:setImage(0, 0, BlockSize, BlockSize, 0, 161, 0, 161, 10.0);
@@ -162,18 +162,54 @@ function DrawMap()
 				end		
 				x=x+BlockSize;			
 			end	
-			x=originX;
-			y=y+BlockSize;	
-		end		
+		x=originX;
+		y=y+BlockSize;	
+	end			
 end
 
 --1代表向右 2代表向上 3代表向左 4代表向下
 function DrawActor()
-	if(movestatus == 0) then
-		--actor:setImage(500, 500, BlockSize, BlockSize, 0, 208, 0, 184, 9.0);
+	NowActorPosX = actorinf:getAbsolutePosX();
+	NowActorPosY = actorinf:getAbsolutePosY();
+	NeedISRevise = false;
+	
+	if releasestatus == 1 then
+	
+		
 	end
-	if movestatus == 1 and actorinf:getAbsolutePosX()+48 < TotalWidthPixels then		
-		if ImpactChecking(actorinf:getAbsolutePosX()+48+UnitXOffset,actorinf:getAbsolutePosY(),1) then
+	if releasestatus == 2 then
+		RemainNum = (NowActorPosY)%BlockSize;
+		if 20 <= RemainNum  then
+			NeedISRevise = true;
+		else NeedISRevise = false;		
+		end
+		if originY>-BlockSize*(TotalRows-12) and actorinf:getWindowPosY()>=9*BlockSize then
+			if NeedISRevise then
+					originY=originY-(BlockSize-RemainNum);
+					actorinf:setRelativePos(0,BlockSize-RemainNum);
+					actorimg:setRelativelyStartPos(0,0);
+			end		
+		else 
+			if NeedISRevise then
+					actorimg:setRelativelyStartPos(0,50-RemainNum);	
+					actorinf:moveTOALL(0,50-RemainNum);					
+			end				
+		end		
+					
+	end			
+	
+	--[[if(movestatus == 0) then
+				
+	end--]]
+	
+	
+	
+	
+	
+	
+	
+	if movestatus == 1 and NowActorPosX+50 < TotalWidthPixels then		
+		if ImpactChecking(NowActorPosX+UnitXOffset,NowActorPosY,1) then
 			if originX>-BlockSize*(TotalLines-20) and actorinf:getWindowPosX()>=16*BlockSize then
 				originX=originX-UnitXOffset;
 				actorimg:setRelativelyStartPos(0,0);
@@ -185,66 +221,169 @@ function DrawActor()
 		end			
 			
 	end
-	if movestatus == 2 and actorinf:getAbsolutePosY() < TotalHeightPixels then
-		if originY>-BlockSize*(TotalRows-12) and actorinf:getWindowPosY()>=9*BlockSize then
-			originY=originY-UnitYOffset;
-			actorimg:setRelativelyStartPos(0,0);
-			actorinf:setRelativePos(0,UnitYOffset);	
-		else 
-			actorimg:setRelativelyStartPos(0,UnitYOffset);	
-			actorinf:moveTOALL(0,UnitYOffset);		
-		end		
+	if movestatus == 2 and NowActorPosY < TotalHeightPixels then
+		if ImpactChecking(NowActorPosX,NowActorPosY+UnitYOffset,2) then
+			if originY>-BlockSize*(TotalRows-12) and actorinf:getWindowPosY()>=9*BlockSize then
+				originY=originY-UnitYOffset;
+				actorimg:setRelativelyStartPos(0,0);	
+				actorinf:setRelativePos(0,UnitYOffset);	
+			else
+				actorimg:setRelativelyStartPos(0,UnitYOffset);	
+				actorinf:moveTOALL(0,UnitYOffset);	
+			end
+		end
+		
+			
 	end
-	if movestatus == 3 and actorinf:getAbsolutePosX() > 0 then
-		if originX<0 and actorinf:getWindowPosX()<=3*BlockSize then
-			originX=originX+UnitXOffset;
-			actorimg:setRelativelyStartPos(0,0);
-			actorinf:setRelativePos(-UnitXOffset,0);		
-		else 
-			actorimg:setRelativelyStartPos(-UnitXOffset,0);
-			actorinf:moveTOALL(-UnitXOffset,0);
-		end			
+	if movestatus == 3 and NowActorPosX > 0 then
+		if ImpactChecking(NowActorPosX-UnitXOffset,NowActorPosY,3) then
+			if originX<0 and actorinf:getWindowPosX()<=3*BlockSize then
+				originX=originX+UnitXOffset;
+				actorimg:setRelativelyStartPos(0,0);
+				actorinf:setRelativePos(-UnitXOffset,0);		
+			else 
+				actorimg:setRelativelyStartPos(-UnitXOffset,0);
+				actorinf:moveTOALL(-UnitXOffset,0);
+			end	
+		end
+				
 	end
-	if movestatus == 4 and actorinf:getAbsolutePosY()-48 > 0 then
-		if originY<0 and actorinf:getWindowPosY()<=4*BlockSize then
-			originY=originY+UnitYOffset;
-			actorimg:setRelativelyStartPos(0,0);
-			actorinf:setRelativePos(0,-UnitYOffset);	
-		else	
-			actorimg:setRelativelyStartPos(0,-UnitYOffset);
-			actorinf:moveTOALL(0,-UnitXOffset);	
-		end			
+	if movestatus == 4 and NowActorPosY-50 > 0 then
+		if ImpactChecking(NowActorPosX,NowActorPosY-UnitYOffset,4) then
+			if originY<0 and actorinf:getWindowPosY()<=4*BlockSize then
+				originY=originY+UnitYOffset;
+				actorimg:setRelativelyStartPos(0,0);
+				actorinf:setRelativePos(0,-UnitYOffset);	
+			else	
+				actorimg:setRelativelyStartPos(0,-UnitYOffset);
+				actorinf:moveTOALL(0,-UnitYOffset);	
+			end	
+		end				
 	end
 	actorimg:DrawImage();
 end
 	
 function ImpactChecking(CheckingPosX,CheckingPosY,Direction)
 	
+	--[[if CheckingPosX <= 0  or  CheckingPosX >= TotalWidthPixels 
+	or CheckingPosY <=0 or CheckingPosY >= TotalHeightPixels then
+		return false;
+	end--]]
 	
 	local CheckingX1;
 	local CheckingY1;
 	local CheckingX2;
 	local CheckingY2;
+	local CheckingXRemainder;
+	local CheckingYRemainder;
 	if Direction == 1 then
-		--[[if 48 <= CheckingPosX % BlockSize <= 50 then  --判断是否肯定不会碰撞
+		CheckingPosX = CheckingPosX+50;
+		CheckingXRemainder = CheckingPosX % BlockSize;
+		--[[if (45 <= CheckingXRemainder and CheckingXRemainder < 50) or CheckingXRemainder == 0 then  --判断是否肯定不会碰撞
 			return true;
 		end--]]
 		CheckingX1 = math.ceil(CheckingPosY/BlockSize);
 		CheckingY1 = math.ceil(CheckingPosX/BlockSize);
-		CheckingRemainder = CheckingPosY % BlockSize;
-		if  48 <= CheckingRemainder or CheckingRemainder < 50 or CheckingRemainder-50 == 0 then  --判断人物是否在一个方格内
+		CheckingYRemainder = CheckingPosY % BlockSize;
+		if  --[[(45 <= CheckingYRemainder and CheckingYRemainder < 50) or --]]CheckingYRemainder == 0 then  --判断人物是否在一个方格内
 			if mapTable[CheckingX1][CheckingY1][3] == 1 or  mapTable[CheckingX1][CheckingY1][4] == 1 then
 				return false;
 			else return true;			
-			end					
+			end	
+			return true;				
 		else 
-			CheckingY2 = CheckingY1-1;
-			if mapTable[CheckingX1][CheckingY1][3] == 1 or mapTable[CheckingX1][CheckingY1][4] == 1
-			or mapTable[CheckingX1][CheckingY2][3] == 1 or mapTable[CheckingX1][CheckingY2][4] == 1 then
+			--return false;
+			CheckingX2 = CheckingX1-1;
+			if mapTable[CheckingX1][CheckingY1][3] == 1 or mapTable[CheckingX1][CheckingY1][4] == 1 then
 				return false;
+			elseif mapTable[CheckingX2][CheckingY1][3] == 1 or mapTable[CheckingX2][CheckingY1][4] == 1 then
+				return false;			
 			else return true;
 			end
 		end	
+		
+		
+	elseif Direction == 2 then	
+		CheckingX1 = math.ceil(CheckingPosY/BlockSize);	
+		CheckingXRemainder = CheckingPosX % BlockSize;
+		if CheckingXRemainder == 0 then
+			CheckingY1 = math.ceil(CheckingPosX/BlockSize)+1;
+			if mapTable[CheckingX1][CheckingY1][3] == 1 or  mapTable[CheckingX1][CheckingY1][4] == 1 then
+				return false;
+			else return true;			
+			end	
+		else
+			CheckingY1 = math.ceil(CheckingPosX/BlockSize);
+			CheckingY2 = CheckingY1+1;
+			if mapTable[CheckingX1][CheckingY1][3] == 1 or mapTable[CheckingX1][CheckingY1][4] == 1 then
+				return false;
+			elseif mapTable[CheckingX1][CheckingY2][3] == 1 or mapTable[CheckingX1][CheckingY2][4] == 1 then
+				return false;			
+			else return true;
+			end		
+		end
+		
+	elseif Direction == 3 then
+		CheckingXRemainder = CheckingPosX % BlockSize;
+		if  CheckingXRemainder == 0 then
+			CheckingY1 = math.ceil(CheckingPosX/BlockSize)+1;	
+		else CheckingY1 = math.ceil(CheckingPosX/BlockSize);
+		end					
+		CheckingYRemainder = CheckingPosY % BlockSize;
+		if CheckingYRemainder == 0 then
+			CheckingX1 = math.ceil(CheckingPosY/BlockSize);	
+			if mapTable[CheckingX1][CheckingY1][3] == 1 or  mapTable[CheckingX1][CheckingY1][4] == 1 then
+				return false;
+			else return true;			
+			end	
+		else 
+			CheckingX1 = math.ceil(CheckingPosY/BlockSize);
+			CheckingX2 = CheckingX1-1;
+			if mapTable[CheckingX1][CheckingY1][3] == 1 or mapTable[CheckingX1][CheckingY1][4] == 1 then
+				return false;
+			elseif mapTable[CheckingX2][CheckingY1][3] == 1 or mapTable[CheckingX2][CheckingY1][4] == 1 then
+				return false;			
+			else return true;
+			end
+		end
+		
+	elseif Direction == 4 then
+		CheckingPosY = CheckingPosY-50;
+		
+		CheckingX1 = math.ceil(CheckingPosY/BlockSize);	
+		CheckingXRemainder = CheckingPosX % BlockSize;
+		
+		if CheckingXRemainder == 0 then
+			CheckingY1 = math.ceil(CheckingPosX/BlockSize)+1;
+			if mapTable[CheckingX1][CheckingY1][3] == 1 or  mapTable[CheckingX1][CheckingY1][4] == 1 then
+				return false;
+			else return true;			
+			end
+		else
+			CheckingY1 = math.ceil(CheckingPosX/BlockSize);
+			CheckingY2 = CheckingY1+1;
+			if mapTable[CheckingX1][CheckingY1][3] == 1 or mapTable[CheckingX1][CheckingY1][4] == 1 then
+				return false;
+			elseif mapTable[CheckingX1][CheckingY2][3] == 1 or mapTable[CheckingX1][CheckingY2][4] == 1 then
+				return false;			
+			else return true;
+			end		
+		end
+		
+	
+	end
+		
+		
+		
+		
+		
+		--[[CheckingXRemainder = CheckingPosX % BlockSize;
+		
+		if CheckingXRemainder == 0
+		
+		
+		
+	end--]]
 	--[[elseif Direction == 2 then
 		CheckingX1 = math.ceil(CheckingPosX/BlockSize)+1;
 		CheckingY1 = math.ceil(CheckingPosY/BlockSize)+1;
@@ -254,21 +393,42 @@ function ImpactChecking(CheckingPosX,CheckingPosY,Direction)
 	elseif Direction == 4 then
 		CheckingX1 = math.ceil(CheckingPosX/BlockSize)+1;
 		CheckingY1 = math.ceil(CheckingPosY/BlockSize);--]]
-	end	
+end	
 	
 	--[[if mapTable[CheckingX1][CheckingY1][3] == 1 or mapTable[CheckingX1][CheckingY1][4] == 1 then
 		return false;
 	else return true;	
 	end--]]
 	
-	
-end	
+	--[[return true;
+end	--]]
 	
 	
 function LoadMapViewImageFile()
-	ground:LoadImage("Image/ground.jpg","DrawGround()", "Image_0");
-	BrickWall:LoadImage("Image/brick.png","DrawMap()", "Image_1");
+	ground:LoadImage("Image/ground3.png","DrawGround()", "Image_0");
+	BrickWall:LoadImage("Image/brick4.jpg","DrawMap()", "Image_1");
 	actorimg:LoadImage("Image/actor.png","DrawActor()", "Image_2");
+	
+end
+
+
+function ActorKeyFluidRelease()
+	local KeyResult_right = KeyDetect(Right);
+	local KeyResult_Up = KeyDetect(Up);
+	local KeyResult_Left = KeyDetect(Left);
+	local KeyResult_Down = KeyDetect(Down);	
+	
+	if KeyResult_right == Release then
+		releasestatus = 1;
+	elseif KeyResult_Up == Release then
+		releasestatus = 2;
+	elseif KeyResult_Left == Release then
+		releasestatus = 3;
+	elseif KeyResult_Down == Release then
+		releasestatus = 4;
+	else
+		releasestatus = 0;
+	end
 	
 end
 
@@ -282,8 +442,12 @@ function ActorKey()
 	local KeyResult_Down = KeyDetect(Down);
 	if KeyResult_right == KeepPressing then
 		movestatus = 1;
+	--[[elseif KeyResult_right == Release then
+		releasestatus = 1;--]]
+	--[[elseif KeyResult_Up == KeepPressing then
+		movestatus = 2;--]]
 	elseif KeyResult_Up == KeepPressing then
-		movestatus = 2;
+		movestatus = 2;--releasestatus = 2;
 	elseif KeyResult_Left == KeepPressing then
 		movestatus = 3;
 	elseif KeyResult_Down == KeepPressing then
@@ -291,6 +455,26 @@ function ActorKey()
 	else
 		movestatus = 0;
 	end
+	
+	--[[if KeyResult_right == Press then
+		movestatus = 1;
+	end
+	if KeyResult_right == Release then
+		movestatus = 0;
+	end
+	
+	if KeyResult_Left == Press then
+		movestatus = 3;
+	end
+	if KeyResult_Left == Release then
+		movestatus = 0;
+	end
+	if KeyResult_Down == Press then
+		movestatus = 4;
+	end
+	if KeyResult_Down == Release then
+		movestatus = 0;
+	end--]]
 	
 end	
 
