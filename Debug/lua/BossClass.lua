dd2f5edba00d3b6a49a6b1705e6d42ae13a0f85e
Mikuling <wangzhezdt@gmail.com>
2017-06-11 11:39:10 +0800

    Boss路径未完成，已注释，别删

--- Debug/lua/BossClass.lua
+++ Debug/lua/BossClass.lua
@@ -1,25 +1,30 @@
 BossClass = 
 {
 	AcStPosX = 500;
-	AcStPosY = 400;
+	AcStPosY = 1100;
 }
 
-function BossClass:move(mx,my)
+function BossClass:Bossmove(mx,my)
 	self.AcStPosX = self.AcStPosX+mx;
 	self.AcStPosY = self.AcStPosY+my;
 end
 
-function BossClass:getAbsolutePosX()
+function BossClass:getBossAbsolutePosX()
 	return self.AcStPosX;
 end
 
-function BossClass:getAbsolutePosY()
+function BossClass:getBossAbsolutePosY()
 	return self.AcStPosY;
 end
 
-function BossClass:setAbsolutePos(ax,ay)
+function BossClass:setBossAbsolutePos(ax,ay)
 	self.AcStPosX = ax;
 	self.AcStPosY = ay;
+end
+
+function BossClass:setBossRelativePos(sx,sy)
+	self.AcStPosX = self.AcStPosX+sx;
+	self.AcStPosY = self.AcStPosY+sy;
 end
 
 function BossClass:new()
